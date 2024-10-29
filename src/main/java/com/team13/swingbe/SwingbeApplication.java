25be26d701ca90d301b940da11895272b889dc09
2rjswn <h47841930@gmail.com>
2024-10-29 19:57:05 +0900

    start

---
+++ src/main/java/com/team13/swingbe/SwingbeApplication.java
@@ -0,0 +1,13 @@
+package com.team13.swingbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwingbeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwingbeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team13/swingbe/SwingbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team13.swingbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwingbeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
