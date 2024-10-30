d7298bad80bd22dc259f430974b945b368965f03
HyunSu1768 <azxcv1768@gmail.com>
2024-10-30 18:00:59 +0900

    feat(UserController): include user role in UserInfoResponse to provide more user details feat(UserInfoResponse): add role field to UserInfoResponse for enhanced user information

--- src/main/java/com/team13/swingbe/domain/user/controller/dto/response/UserInfoResponse.java
+++ src/main/java/com/team13/swingbe/domain/user/controller/dto/response/UserInfoResponse.java
@@ -2,5 +2,6 @@ package com.team13.swingbe.domain.user.controller.dto.response;
 
 public record UserInfoResponse(
         String name,
-        String number
+        String number,
+        String role
 ) {}
